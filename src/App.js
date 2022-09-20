77434dff1ae1d70a97f316832621787f2474428c
darkodimovski <darkodimovski57@gmail.com>
2022-09-20 17:52:59 +0200

    tailwind navbar

--- src/App.js
+++ src/App.js
@@ -7,17 +7,31 @@ import { ListaNaloga } from "./components/ListaNaloga";
 function App() {
   return (
     <>
-      <div>
-      <li>
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded-b dark:bg-gray-900 mb-20">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="https://www.printecgroup.com/" className="flex items-center">
+          <img src="https://www.printecgroup.com/wp-content/uploads/2019/07/printec_logo_white.svg" className="mr-3 h-6 sm:h-12" alt="Printec logo" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+        </a>
+      <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <span className="sr-only">Open main menu</span>
+      <svg className="w-6 h-6" aria-hidden="true" fillRule="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+     </button>
+    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
           <Link to='/'>Home</Link>
         </li>
-        <li>
+        <li className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
           <Link to='/unos-naloga'>Unos Naloga</Link>
         </li>
-        <li>
+        <li className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
           <Link to='/lista-naloga'>Lista Naloga</Link>
         </li>
-      </div>
+      </ul>
+    </div>
+  </div>
+</nav>       
       <Routes>
         <Route path ="/" element={<Home />} />
         <Route path="/unos-naloga" element={<UnosNaloga />} />
