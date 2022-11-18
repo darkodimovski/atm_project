a261611a5ef9a01bad4293bbcb50a8951f9df961
darkodimovski <darkodimovski57@gmail.com>
2022-11-18 14:19:57 +0100

    edit forma working

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Navbar from "./components/layout/Navbar";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
 import AddNalog from "./components/nalozi/AddNalog";
-import EditNalog from "./components/nalozi/EditNalog";
 import Nalog from "./components/nalozi/Nalog";
+import EditNalog from "./components/nalozi/EditNalog";
+import { EditNalogNew } from "./components/nalozi/EditNalogNew";
 
 
 function App() {
   return (
       <>
         <Navbar />
-
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/about" exact element={<About />} />
           <Route path="/contact" exact element={<Contact />} />
           <Route path="/nalozi/add" exact element={<AddNalog />} />
-          <Route path="/api/tests/edit/:id"  exact element={<EditNalog />} />
+          <Route path="/api/tests/:id" exact element={<EditNalog />} />
           <Route path="/api/tests/view/:id" exact element={<Nalog />} />
           <Route element={<NotFound />} />
         </Routes>
